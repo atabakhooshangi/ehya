1db3cfa485ceb6b7090b2458fbc4aa26da9cfe7d
atabakhooshangi <atabak.hooshangi.1990@gmail.com>
2021-12-21 22:52:24 +0330

    s

--- wphome/serializers.py
+++ wphome/serializers.py
@@ -12,7 +12,7 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = WpComments
         fields = [
-            'comment_id',
+            'id',
             'comment_post_id',
             'comment_author',
             'comment_author_email',
