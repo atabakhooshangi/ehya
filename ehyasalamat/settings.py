f278c7f2fe8267709712b6ceffeca6a5900b194c
atabak-hooshangi <doom_horn@yahoo.com>
2022-04-02 12:21:52 +0430

    settings was a mess

--- ehyasalamat/settings.py
+++ ehyasalamat/settings.py
@@ -8,7 +8,7 @@ from firebase_admin import credentials
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development test_settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
